905a5d6a5f8aebc5e8116b16b7f8766519dc5b7a
tkovs <tkovs@outlook.com>
2016-09-16 13:33:35 -0300

    Novo campo na tabela de problema

--- translation_portuguese/models.py
+++ translation_portuguese/models.py
@@ -4,3 +4,4 @@ class Question(models.Model):
     number      = models.PositiveSmallIntegerField()
     name        = models.TextField()
     enunciation = models.TextField()
+    solved_by   = models.PositiveSmallIntegerField(default = 0)
